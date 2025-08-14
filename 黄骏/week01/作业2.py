f2103eab2ffd091bb490e7b77abbbb5a1b2f374a
Yun <tcyyhuangstone@163.com>
2025-08-14 16:27:20 +0800

    修改完善

--- 黄骏/week01/作业2.py
+++ 黄骏/week01/作业2.py
@@ -1,28 +1,34 @@
 import  pandas as pd
 import jieba
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
 # 导入数据集
 dataset = pd.read_csv("dataset.csv", sep = "\t", header=None)
-# 数据处理
-input_sentence = dataset[0].apply(lambda x: " ".join(jieba.lcut(x)))
-# 进行特征提取
+# 数据集划分
+X = dataset[0]
+y = dataset[1]
+tran_x, text_x, tran_y, text_y = train_test_split(X, y, random_state=666)
+# 训练数据处理
+tran_sentence = tran_x.apply(lambda x: " ".join(jieba.lcut(x)))
+# 训练数据特征提取
 vectorizer = CountVectorizer()
-input_feature = vectorizer.fit_transform(input_sentence.values)
+vectorizer.fit(tran_sentence.values)
+tran_feature = vectorizer.transform(tran_sentence.values)
 # 训练模型
 model1 = KNeighborsClassifier() # KNN模型
-model1.fit(input_feature, dataset[1].values)
+model1.fit(tran_feature, tran_y.values)
 model2 = LogisticRegression()   # 逻辑回归模型
-model2.fit(input_feature, dataset[1].values)
-# 测试文本处理
-test_query = input("待预测的文本：")
-test_sentence = " ".join(jieba.lcut(test_query))
-test_feature = vectorizer.transform([test_sentence])
+model2.fit(tran_feature, tran_y.values)
+# 测试数据处理
+test_sentence = text_x.apply(lambda x: " ".join(jieba.lcut(x)))
+# 测试数据特征提取
+test_feature = vectorizer.transform(test_sentence.values)
 # 模型推理
 prediction1 = model1.predict(test_feature)
 prediction2 = model2.predict(test_feature)
-
-print("KNN模型的预测结果：", prediction1)
-print("逻辑回归模型的预测结果：", prediction2)
+# 预测结果
+print(f"KNN模型的预测结果：{(prediction1 == text_y).sum()}/{len(text_y)}")
+print(f"KNN模型的预测结果：{(prediction2 == text_y).sum()}/{len(text_y)}")
